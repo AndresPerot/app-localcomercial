229afc93c8ba7fd32bb80c6923d8e933201bd444
Andres Perot <andres.perot.quevedo@ciisa.cl>
2022-10-28 17:57:34 -0300

    login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class UserController extends Controller
+{
+    public function create(Request $request){
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->lastname = $request->input('lastname');
+        $user->rut = $request->input('rut');
+        $user->contact = $request->input('contact');
+        $user->position = $request->input('position');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        return json_encode(["msg" => "usuario agregado"]); 
+        return redirect()->intended('/home');
+    }
+}
---
+++ resources/views/register.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    @include('partials/head')
+    <body>
+        @include('partials/navbar')
+         <div class="container p-4">
+            <form action="register" method="POST" class="card border-info">
+                @csrf 
+                
+                <div class="card-body">
+                 <input 
+                    type = "text"
+                    name = "name"
+                    class = "form-control mb-2"
+                    placeholder = "Nombre..." 
+                 />
+                 
+                 <input 
+                    type = "text"
+                    name = "lastname"
+                    class ="form-control mb-2"
+                    placeholder = "Apellido..." 
+                 />
+                 
+                 <input 
+                    type = "text"
+                    name = "rut"
+                    class = "form-control mb-2"
+                    placeholder = "Rut..." 
+                 />
+                 
+                 <input 
+                    type = "text"
+                    name = "contact"
+                    class ="form-control mb-2"
+                    placeholder = "Contacto..." 
+                 />
+
+                 <input 
+                    type = "text"
+                    name = "position"
+                    class = "form-control mb-2"
+                    placeholder = "Cargo..." 
+                 />
+
+                 <input 
+                    type = "email"
+                    name = "email"
+                    class = "form-control mb-2"
+                    placeholder = "Email..." 
+                 />
+                 
+                 <input 
+                    type = "password"
+                    name = "password"
+                    class ="form-control mb-2"
+                    placeholder = "Password..." 
+                 />
+                 <button 
+                   class ="btn btn-info"
+                   >Crear Usuario
+                 </button>
+                </div>
+             </div>  
+            
+        </div>
+    </body>
+</html>
